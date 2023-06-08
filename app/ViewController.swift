f08fd9fa7f659bb6b179a092e0071c40a05ab58e
Vitaly Belikov <vitalybelikov@MacBook-Air-Vitaly.local>
2023-06-08 11:38:07 +0300

    Init commit

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Vitaly Belikov on 05.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
